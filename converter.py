e05309cd1d205b4e4ee1e13633a55da5a10b2ba1
Tom Elliff <tomelliff@gmail.com>
2017-05-02 14:14:35 +0100

    Convert CIDR ranges into classful ranges

--- converter.py
+++ converter.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
+import netaddr
 
 
 def converter(cidr_range):
-    return [cidr_range]
+    allowed_prefixes = [8, 16, 24, 32]
+    ip = netaddr.IPNetwork(cidr_range)
+    cidr_prefix = ip.prefixlen
+    if cidr_prefix in allowed_prefixes:
+        return [cidr_range]
+    else:
+        for prefix in allowed_prefixes:
+            if cidr_prefix < prefix:
+                subnets = []
+                for subnet in ip.subnet(prefix):
+                    subnets.append(str(subnet))
+                return subnets
--- test_converter.py
+++ test_converter.py
@@ -8,6 +8,13 @@ class TestConvertClassless(unittest.TestCase):
         self.assertEqual(converter.converter('192.168.0.0/24'),
                          ['192.168.0.0/24'])
 
+    def test_classless_returns_classes(self):
+        self.assertEqual(converter.converter('192.168.0.0/23'),
+                         ['192.168.0.0/24', '192.168.1.0/24'])
+        self.assertEqual(converter.converter('192.168.0.0/30'),
+                         ['192.168.0.0/32', '192.168.0.1/32',
+                          '192.168.0.2/32', '192.168.0.3/32'])
+
 
 if __name__ == '__main__':
     unittest.main()
