1afd08f5015707602f646b4e3f127a6fe5245fb5
Tom Elliff <tomelliff@gmail.com>
2017-05-02 14:43:01 +0100

    Refactor generator to list into separate helper function
    ip.subnet(prefix) returns a generator of IPNetwork objects but we want a list.
    Using list(ip.subnet(prefix)) returns a list of IPNetwork objects but we want the string representation here.
    So instead we iterate through the generator and build a list of the string representation of each IPNetwork object.

--- converter.py
+++ converter.py
@@ -2,6 +2,16 @@
 import netaddr
 
 
+def _generator_to_list(subnets_generator):
+    """Returns list of string representations
+    of each yielded item from the generator.
+    """
+    subnets = []
+    for subnet in subnets_generator:
+        subnets.append(str(subnet))
+    return subnets
+
+
 def converter(cidr_range):
     allowed_prefixes = [8, 16, 24, 32]
     ip = netaddr.IPNetwork(cidr_range)
@@ -11,7 +21,4 @@ def converter(cidr_range):
     else:
         for prefix in allowed_prefixes:
             if cidr_prefix < prefix:
-                subnets = []
-                for subnet in ip.subnet(prefix):
-                    subnets.append(str(subnet))
-                return subnets
+                return _generator_to_list(ip.subnet(prefix))
--- test_converter.py
+++ test_converter.py
@@ -1,5 +1,7 @@
 import unittest
 
+import netaddr
+
 import converter
 
 
@@ -14,6 +16,14 @@ class TestConvertClassless(unittest.TestCase):
         self.assertEqual(converter.converter('192.168.0.0/30'),
                          ['192.168.0.0/32', '192.168.0.1/32',
                           '192.168.0.2/32', '192.168.0.3/32'])
+
+
+class TestSubnetGeneratorToList(unittest.TestCase):
+    subnets_generator = netaddr.IPNetwork('192.168.0.0/23').subnet(24)
+
+    def test_returns_list_of_strings(self):
+        self.assertEqual(converter._generator_to_list(self.subnets_generator),
+                         ['192.168.0.0/24', '192.168.1.0/24'])
 
 
 if __name__ == '__main__':
