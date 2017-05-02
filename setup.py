0186d674cc4a0152e87fa6556d300f3327d531ff
Tom Elliff <tomelliff@gmail.com>
2017-05-02 17:26:10 +0100

    Add tox support
    Just run tox to automatically run unit tests on both Python2.7 and Python 3.6

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='aws_waf_cidr_converter',
+      version='1.0',
+      description='Converts CIDR ranges into ranges allowed by AWS WAF IPSets',
+      author='Tom Elliff',
+      author_email='tomelliff@gmail.com',
+      )
